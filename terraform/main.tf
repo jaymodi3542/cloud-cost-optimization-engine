fb151c7029549f4e85f82b9772068dbb36246ffb
Jay Modi <62894960+jaybmodi@users.noreply.github.com>
2026-03-16 00:32:34 -0400

    cleared everything from terraform files as well

--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "terraform_test_bucket" {
-  bucket = "jaymodi-cloud-cost-opt-test-001"
-
-  tags = {
-    Project = "cloud-cost-optimization-engine"
-    Owner   = "Jay"
-    Env     = "dev"
-  }
-}
